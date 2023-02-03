d7454001a62b902e4709eddab03c0d81b491fe89
sachnun <sachnun12@gmail.com>
2023-02-04 02:40:09 +0700

    update landing.blade.php and permohonan.blade.php

--- resources/views/permohonan.blade.php
+++ resources/views/permohonan.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - PPDK</title>
+    <title>Penganjuan Permohonan - PPDK</title>
 
     <!-- Custom fonts for this template-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" rel="stylesheet"
